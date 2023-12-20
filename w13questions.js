6716c532a6f75d63ea1db21d3e9a4ae018c3571c
garozaev <g.a.rozaev@gmail.com>
2023-12-20 18:56:46 +0300

    finish2

--- w13questions.js
+++ w13questions.js
@@ -27,7 +27,7 @@
   //console.log("Возраст: " + age + " лет");
 
   // это то же, только currentDate создаёт метод Date.now
-  /*function calculateAge(birthDate) {
+  function calculateAge(birthDate) {
     let currentDate = Date.now();
     console.log(currentDate);
     let diffInMilliseconds = currentDate - birthDate;
@@ -42,58 +42,58 @@
   
   let birthDate = new Date('1990-05-15');
   let age = calculateAge(birthDate);
-  console.log("Возраст: " + age + " лет");*/
+  console.log("Возраст: " + age + " лет");
 
 // сделал короче
 
-function calculateAge(birthDate) {
+function calculateAge1(birthDate1) {
     let currentDate = new Date().getUTCFullYear();
-    let age = currentDate - birthDate;
-    return age;
+    let age1 = currentDate - birthDate1;
+    return age1;
   }
   
-  let birthDate = new Date('1990-05-15').getUTCFullYear();
-  let age = calculateAge(birthDate);
-  console.log("Возраст: " + age + " лет");
+  let birthDate1 = new Date('1990-05-15').getUTCFullYear();
+  let age1 = calculateAge1(birthDate1);
+  console.log("Возраст: " + age1 + " лет");
 
 
 
 // 1 задание 
 
-/*let newDate0 = new Date (2021,0,24,22,51);
+let newDate0 = new Date (2021,0,24,22,51);
 let newDate1 = new Date('2021-01-24T22:51');
 console.log(newDate0);
-console.log(newDate1);*/
+console.log(newDate1);
 
 // 3 Задание 
 
-/*let now = new Date();
+let now = new Date();
 let tomorrow = new Date(now.getFullYear(),now.getMonth(),now.getDate() + 1);
 let differenceMs = tomorrow - now;
 let differenceSec = Math.floor(differenceMs / 1000 );
 
-console.log(differenceSec);*/
+console.log(differenceSec);
 
 // 5 задание
 
-/*let d = new Date(2016, 2, 9);
-console.log(d);*/
+let d = new Date(2016, 2, 9);
+console.log(d);
 
 // 7 Задание
 
-/*let date = new Date();
-console.log(date.getUTCHours());*/
+let date = new Date();
+console.log(date.getUTCHours());
 
 // 9 Задание
 
-/*let d = new Date();
-let y = d.getFullYear();
-console.log(y);*/
+let d1 = new Date();
+let y1 = d1.getFullYear();
+console.log(y1);
 
 // 10 Задание
 
-/*let y = new Date();
-let d = y.getDate();
-console.log(d);*/
+let y2 = new Date();
+let d2 = y2.getDate();
+console.log(d2);
 
 
