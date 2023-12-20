4a96cee9462fda103a9dc124801312f3ecff9cf6
garozaev <g.a.rozaev@gmail.com>
2023-12-20 18:28:15 +0300

    finish

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 1. Как создать дату 24 января 2021 года, 22 часа 51 минута? Временная зона — местная.
 
 let newDate0 = new Date (2021,0,24,22,51);
-let newDate1 = new Date(2021-0-24);
+let newDate1 = new Date('2021-01-24');
 console.log(newDate0);
 console.log(newDate1);
 
@@ -13,18 +13,27 @@ console.log(newDate1);
 
 3. Как посчитать, сколько секунд осталось до завтра?
 
-let nowDate = Date.now();
-let tomorrow =
+let now = new Date();
+let tomorrow = new Date(now.getFullYear(),now.getMonth(),now.getDate() + 1);
+let differenceMs = tomorrow - now;
+let differenceSec = Math.floor(differenceMs / 1000 );
+
+console.log(differenceSec);
 
 4. Для чего предназначен метод getDate()?
+
+- ьетод getDate предназначен для извлечения информации о дне месяца (от 1 до 31) из объекта Date
 
 5. Что выведет console.log(d) ?
 
    let d = new Date(2016, 2, 9);
-
    console.log(d);
 
+   - выведет дату 9 марта 2016г 00:00:00 местного времени Wed Mar 09 2016 00:00:00 GMT+0300 (Москва, стандартное время)
+
 ​6. Что делает getTimezoneOffset() ?
+
+- показывает разницу между местным в ременем и UTC + 0 в минутах
 
 7. Что выведет консоль?
 
@@ -32,14 +41,24 @@ let date = new Date();
 
 console.log(date.getUTCHours());
 
-​8. Для чего предназначен метод getHours() объекта Date? 9. Что выведет консоль?
+- выведет выведет время(час) по UTC шкале в зоне UTC+0(лондонское время без учёта летнего)
+
+​8. Для чего предназначен метод getHours() объекта Date?
+
+- getHours() предназначен для получения значения часа в местном часовом поясе из объекта Date
+
+9. Что выведет консоль?
 
 let d = new Date();
 let y = d.getFullYear();
 console.log(y);
+
+- выведет значение года в местном часовом поясе
 
 ​10. В чём ошибка в коде?
 
 let y = new Date();
 let d = y.getDate();
 console.log(d);
+
+- ошибки нет
--- w13questions.js
+++ w13questions.js
@@ -1,13 +1,99 @@
-/*let newDate0 = new Date (2021,0,24,22,51);
-let newDate1 = new Date(2021-0-24);
-//let newDate2 = getTime(2021,0,24,22,51);
-console.log(newDate0);
-console.log(newDate1);
-//console.log(newDate2);*/
+// просто тренировка
+// попробовал сделать короче
+// не понял зачем Math.abs
+/*не совсем понял как пересчитываутя разница
+в милсек(diffInMilliseconds) в год(2003),
+получается пересчитывает в дату?!, 
+поэтому приходится вычитать 1970?!*/
+/*прикольно, если задать текущую дату и дату рождения
+методом Date.now то, видно что обработку уходит 1 милисек
+или меньше*/ 
+//function calculateAge(birthDate) {
+    //let currentDate = Date.now();/*.getUTCFullYear();*/
+    //console.log(currentDate);
+    //let diffInMilliseconds = currentDate - birthDate;
+    //console.log(diffInMilliseconds);  
+    // Конвертируем разницу в миллисекундах в годы
+    //let ageDate = new Date(diffInMilliseconds).getUTCFullYear();
+    //console.log(ageDate);
+    //let age = Math.abs(ageDate/*.getUTCFullYear()*/ - 1970);
+    //console.log(age);
+    //return age;
+  //}
+  
+  //let birthDate =/*Date.now()*/new Date('1990-05-15');/*.getUTCFullYear();*/
+  //console.log(birthDate);
+  //let age = calculateAge(birthDate);
+  //console.log("Возраст: " + age + " лет");
 
-let now = new Date();
+  // это то же, только currentDate создаёт метод Date.now
+  /*function calculateAge(birthDate) {
+    let currentDate = Date.now();
+    console.log(currentDate);
+    let diffInMilliseconds = currentDate - birthDate;
+    console.log(diffInMilliseconds);
+    // Конвертируем разницу в миллисекундах в годы
+    let ageDate = new Date(diffInMilliseconds);
+    let age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    console.log(ageDate);
+    console.log(age);
+    return age;
+  }
+  
+  let birthDate = new Date('1990-05-15');
+  let age = calculateAge(birthDate);
+  console.log("Возраст: " + age + " лет");*/
+
+// сделал короче
+
+function calculateAge(birthDate) {
+    let currentDate = new Date().getUTCFullYear();
+    let age = currentDate - birthDate;
+    return age;
+  }
+  
+  let birthDate = new Date('1990-05-15').getUTCFullYear();
+  let age = calculateAge(birthDate);
+  console.log("Возраст: " + age + " лет");
+
+
+
+// 1 задание 
+
+/*let newDate0 = new Date (2021,0,24,22,51);
+let newDate1 = new Date('2021-01-24T22:51');
+console.log(newDate0);
+console.log(newDate1);*/
+
+// 3 Задание 
+
+/*let now = new Date();
 let tomorrow = new Date(now.getFullYear(),now.getMonth(),now.getDate() + 1);
 let differenceMs = tomorrow - now;
 let differenceSec = Math.floor(differenceMs / 1000 );
 
-console.log(differenceSec);
+console.log(differenceSec);*/
+
+// 5 задание
+
+/*let d = new Date(2016, 2, 9);
+console.log(d);*/
+
+// 7 Задание
+
+/*let date = new Date();
+console.log(date.getUTCHours());*/
+
+// 9 Задание
+
+/*let d = new Date();
+let y = d.getFullYear();
+console.log(y);*/
+
+// 10 Задание
+
+/*let y = new Date();
+let d = y.getDate();
+console.log(d);*/
+
+
